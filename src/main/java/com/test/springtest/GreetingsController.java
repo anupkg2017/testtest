13301b307a597a98854c752ed723078c8b288d9e
Anup Kanoor Gopalakrishnan <agopalakrishnan@manh.com>
2019-08-08 10:53:47 -0400

    initial

---
+++ src/main/java/com/test/springtest/GreetingsController.java
@@ -0,0 +1,15 @@
+package com.test.springtest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name) {
+        return "greeting"+name;
+    }
+
+}
